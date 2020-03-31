80592f6b37f3e5b1bcf8669ae00a6fd08ca3b884
hrso94 <hrvoje.medak@gluonsoftware.hr>
2020-03-31 18:04:25 +0200

    Update on services page and css

--- service.php
+++ service.php
@@ -1,7 +1,5 @@
 
-<?php get_header(); 
-/*Template Name: Service */
-?>
+<?php get_header(); ?>
 
 
 <section id="product-design" class="snt-services-page-section">
@@ -20,7 +18,7 @@
 		  <div class="row">
 			<div class="col-12">
 			  <h3 class="yellow">FLOW, FORM and function</h3>
-			  <h1>Product Design </h1>
+			  <h1 class="product-h1">Product Design </h1>
 			</div>
 		  </div>
 		</div>
@@ -34,31 +32,46 @@
 	</div>
 
 	<div class="row">
-		<div class="col-md-4">
+		<div id="product" class="col-md-4 des-ux-int">
 			<div class="col-md-12">
-				<button class="thunder">
+				<button class="thunder" value="showDesign" onclick="showDesign()">
 					<h0> Design Sprint </h0>
 				</button>
 			</div>
 			<div class="col-md-12">
-				<button class="vector">
+				<button class="vector" value="showUx" onclick="showUx()">
 					<h0> UX/UI Design </h0>
 				</button>
 			</div>
 			<div class="col-md-12">
-				<button class="mouse">
+				<button class="mouse" value="showInteraction" onclick="showInteraction()">
 					<h0> Interaction Design </h0>
 				</button>
 			</div>
 		</div>
-		<div class="col-md-8">
+		<div class="col-md-8" id="design">
 			<div class="col-md-12">
 				<h2>DESIGN SPRINT</h2>
 				<p>The Design Sprint is a five-day process for answering critical business questions through design, prototyping, and testing ideas with customers. Developed at Google Ventures, it’s a “greatest hits” of business strategy, innovation, behavior science, design thinking, and more—packaged into a battle-tested process that any team can use.</p>
 				<p> Working together in a sprint, you can shortcut the endless-debate cycle and compress months of time into a single week. Instead of waiting to launch a minimal product to understand if an idea is any good, you’ll get clear data from a realistic prototype. The sprint gives you a superpower: You can fast-forward into the future to see your finished product and customer reactions, before making any expensive commitments. Read More</p>
 				<p> We facilitate Design Sprints on-site or remote.</p>
+			</div>
 		</div>
-	</div>
+		<div class="col-md-8" id="ux" style="display:none;">
+			<div class="col-md-12">
+				<h2>UX/UI DESIGN</h2>
+				<p>User eXperience (UX) design is the process of manipulating user behavior through usability, usefulness, and desirability provided in the interaction with a product. User experience design encompasses traditional human–computer interaction design and extends it by addressing all aspects of a product or service as perceived by users. </p>
+				<p><u>Read more</u></p>
+				<p class="p-ux-design"> "UX design is like telling a joke, if you have to explain it: you’ve failed."</p>
+			</div>
+		</div>
+		<div class="col-md-8" id="interaction" style="display:none;">
+			<div class="col-md-12">
+				<h2>INTERACTION DESIGN</h2>
+				<p>Reprehenderit elit officia reprehenderit commodo consectetur irure adipisicing non ex aliqua adipisicing velit sunt amet. Veniam qui tempor cupidatat Lorem nostrud eu culpa laboris eu.</p>
+				<p> Fugiat occaecat reprehenderit cupidatat fugiat in proident anim pariatur pariatur sunt. Anim reprehenderit fugiat voluptate ex nisi non labore anim proident. Labore magna fugiat qui ad eiusmod.</p>
+			</div>
+		</div>
 
 </section>
 
@@ -72,7 +85,7 @@
 		  <div class="row">
 			<div class="col-12">
 			  <h3 class="yellow">CODE THAT CRUNCHES THE DATA</h3>
-			  <h1>Product Development </h1>
+			  <h1 class="product-h1">Product Development </h1>
 			</div>
 		  </div>
 		</div>
@@ -93,29 +106,43 @@
 	</div>
 
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-4 pro-pro-min">
 			<div class="col-md-12">
-				<button class="plane">
+				<button class="plane" value="showProof" onclick="showProof()">
 					<h0> Proof of Concept </h0>
 				</button>
 			</div>
 			<div class="col-md-12">
-				<button class="airplane">
+				<button class="airplane" value="showPrototype" onclick="showPrototype()">
 					<h0> Prototype </h0>
 				</button>
 			</div>
 			<div class="col-md-12">
-				<button class="rocket">
+				<button class="rocket" value="showMinimum" onclick="showMinimum()">
 					<h0> Minimum Viable Product </h0>
 				</button>
 			</div>
 		</div>
-		<div class="col-md-8">
+		<div class="col-md-8"  id="proof">
 			<div class="col-md-12">
 				<h2>PROOF OF CONCEPT</h2>
 				<p>A Proof of Concept is a small exercise to test the design idea or assumption. The main purpose of developing a POC is to demonstrate the functionality and to verify a certain concept or theory that can be achieved in development.</p>
+			</div>
 		</div>
-	</div>
+		<div class="col-md-8"  id="prototype" style="display:none;">
+			<div class="col-md-12">
+				<h2>PROTOTYPE</h2>
+				<p>While PoC is focused on technical feasibility of the Idea, Prototype focuses more on the usability and desirability. It’s a interactive application that demonstrates the final product.</p>
+				<p> Albeit, the Prototype will not deliver actual functional business logic (nor is this needed at this stage), usable (“clickable”) application is delivered.</p>
+			</div>
+		</div>
+		<div class="col-md-8"  id="minimum" style="display:none;">
+			<div class="col-md-12">
+				<h2>MINIMUM VIABLE PRODUCT</h2>
+				<p>A minimum viable product (MVP) is a concept from Lean Startup that stresses the impact of learning in new product development. Eric Ries, defined an MVP as that version of a new product which allows a team to collect the maximum amount of validated learning about customers with the least effort. This validated learning comes in the form of whether your customers will actually purchase your product.</p>
+				<p> A key premise behind the idea of MVP is that you produce an actual product (which may be no more than a landing page, or a service with an appearance of automation, but which is fully manual behind the scenes) that you can offer to customers and observe their actual behavior with the product or service. Seeing what people actually do with respect to a product is much more reliable than asking people what they would do. Read more</p>
+			</div>
+		</div>
 
 </section>
 
@@ -142,67 +169,85 @@
 		<p> Occaecat laborum culpa mollit tempor reprehenderit enim voluptate eu ex irure minim cillum quis. Aliqua laboris anim voluptate non commodo proident elit. Non adipisicing et culpa minim non esse ut officia commodo laboris exercitation sunt consectetur. Minim reprehenderit cupidatat sunt esse velit do voluptate proident et. </p>
 	</div>
 
-	<div class="row" style="display: inline-block;">
-		<div class="col-md-4">
+	<div class="row consulting-display">
+		<div class="col-md-4 sof-pro-soc-int">
 			<div class="col-md-12">
-				<button class="software">
+				<button class="software" value="showSoftware" onclick="showSoftware()">
 					<h0> Software Development </h0>
 				</button>
 			</div>
 			<div class="col-md-12">
-				<button class="product">
+				<button class="product" value="showProductDesign" onclick="showProductDesign()">
 					<h0> Product Design </h0>
 				</button>
 			</div>
 			<div class="col-md-12">
-				<button class="social">
+				<button class="social" value="showSocial" onclick="showSocial()">
 					<h0> Social Media </h0>
 				</button>
 			</div>
 			<div class="col-md-12">
-				<button class="interim">
+				<button class="interim" value="showInterim" onclick="showInterim()">
 					<h0> Interim CTO </h0>
 				</button>
 			</div>
 		</div>
-		<div class="col-md-8">
+		<div class="col-md-8" id="software">
 			<div class="col-md-12">
 				<h2>SOFTWARE DEV CONSULTING</h2>
-					<p>Building a usable software that’s loved by many is hard, but our experts can guide you along the way, helping you with:</p>
-						<li> <p> Best software development practices, methodologies and procedures </p></li>
-						<li> <p> Agile project life cycle, from roles such as Scrum Master, Product Owner to different ceremonies (Sprint Planning, Demo, Retro) </p></li>
-						<li> <p> Who, when and how to hire developers, designers, testers and other roles </p></li>
-						<li> <p> Security </p></li>
-						<li> <p> Technology </p></li>
+				<p>Building a usable software that’s loved by many is hard, but our experts can guide you along the way, helping you with:</p>
+					<li> <p> Best software development practices, methodologies and procedures </p></li>
+					<li> <p> Agile project life cycle, from roles such as Scrum Master, Product Owner to different ceremonies (Sprint Planning, Demo, Retro) </p></li>
+					<li> <p> Who, when and how to hire developers, designers, testers and other roles </p></li>
+					<li> <p> Security </p></li>
+					<li> <p> Technology </p></li>
+			</div>
 		</div>
-	</div>
+		<div class="col-md-8" id="product-d" style="display:none;">
+			<div class="col-md-12">
+				<h2>PRODUCT DESIGN CONSULTING</h2>
+				<p>Creating an exhilarating application that will be loved and used by many largely depends on the UX/UI design of the application, since that is the first thing users interact with, this is a crucial step to get right from the get go.</p>
+			</div>
+		</div>
+		<div class="col-md-8" id="social" style="display:none;">
+			<div class="col-md-12">
+				<h2>SOCIAL MEDIA CONSULTING</h2>
+				<p>Nowadays your presence on the internet is a paramount. With out a strong, consistent and ever growing social media channels it’s like you don’t even exist. How to gain more followers, how to engage more, how to grow. We can answer all of those.</p>
+			</div>
+		</div>
+		<div class="col-md-8" id="interim" style="display:none;">
+			<div class="col-md-12">
+				<h2>INTERIM CTO CONSULTING</h2>
+				<p>Now days it’s hard to find a technical co-founder. Someone with technical background, life experience and enough games under the belt. While many startups loose valuable time finding the right talent fit, we can jump right in and provide an interim CTO, who can advise the Board on all technical aspects of application development, security, tooling, etc. We can tailor fit the role to your needs.</p>
+			</div>
+		</div>
 
 </section>
 
 <section class="text-center snt-services-page-section">
   <img class="service-page-photo">
     <div class="text-center" style="position:absolute; width:inherit;">
-      <h3 class="contact-h3">TAKE ACTION</h3>
-     <div class="ambassador-section">
+      <h3 class="contact-service-h3">TAKE ACTION</h3>
+     <div class="service-section">
 	  <h2 class="service-h2">You have an abstract software idea that needs some Sintezis?</h2>
 	  <p class="service-page-photo-p">Let's talk about it</p>
-      <button class="ambassador-button"> VIEW OUR WORK</button>
+      <button class="service-button"> VIEW OUR WORK</button>
 	  <button class="snt-get-in-touch-btn"> GET IN TOUCH </button>
       </div>
     </div>
 </section>
 
 <section id="service-travel" class="snt-services-page-section">
-<div class="row">
+	<div class="row">
 	<div class="col-md-10 col-md-offset-1">
 	  
 	  <div class="row service-travel-photo">
 	  
-		<div class="col-md-4"style="border:none; background:none;">
+		<div id=travel class="col-md-4"style="border:none; background: none !important;">
 		  <div class="row">
 			<div class="col-12">
 			  <h3 class="yellow">WHAT WE ARE REALLY GOOD AT</h3>
-			  <h1>Travel Technology </h1>
+			  <h1 class="travel-h1">Travel Technology </h1>
 			  <p>With our extensive knowledge of the travel industry and experience with building various travel tech products/sites, we can get your project of the ground easily.</p>
 			  <p class="travel-text">Our Experience</p>
 			  <p class="travel-p"> We have successfully implemented (and certified) various third party APIs for our clients:</p>
@@ -232,5 +277,160 @@
 </div>
 
 </section>
-  
+
+<script>
+
+function showDesign() {
+	var x = document.getElementById("design");
+	var y = document.getElementById("ux");
+	var z = document.getElementById("interaction");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		y.style.display = "none";
+		z.style.display = "none";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showUx() {
+	var x = document.getElementById("ux");
+	var y = document.getElementById("design");
+	var z = document.getElementById("interaction");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		y.style.display = "none";
+		z.style.display = "none";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showInteraction() {
+	var x = document.getElementById("interaction");
+	var y = document.getElementById("design");
+	var z = document.getElementById("ux");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		y.style.display = "none";
+		z.style.display = "none";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showProof() {
+  var x = document.getElementById("proof");
+  var y = document.getElementById("prototype");
+  var z = document.getElementById("minimum");
+  if (x.style.display === "none") {
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showPrototype() {
+  var x = document.getElementById("prototype");
+  var y = document.getElementById("proof");
+  var z = document.getElementById("minimum");
+  if (x.style.display === "none") {
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none"
+  } else {
+	x.style.display = "none";
+	y.style.display = "block";
+	z.style.display = "none";
+  }
+}
+
+function showMinimum() {
+  var x = document.getElementById("minimum");
+  var y = document.getElementById("prototype");
+  var z = document.getElementById("proof");
+  if (x.style.display === "none") {
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none";
+  } else {
+	x.style.display = "none";
+	y.style.display = "none";
+	z.style.display = "block";
+  }
+}
+
+function showSoftware() {
+  var x = document.getElementById("software");
+  var y = document.getElementById("product-d");
+  var z = document.getElementById("social");
+  var m = document.getElementById("interim");
+  if (x.style.display === "none") {
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none";
+	m.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showProductDesign() {
+  var x = document.getElementById("product-d");
+  var y = document.getElementById("software");
+  var z = document.getElementById("social");
+  var m = document.getElementById("interim");
+  if (x.style.display === "none") {
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none";
+	m.style.display = "none";
+  } else {
+	x.style.display = "none";
+	y.style.display = "block";
+	z.style.display = "none";
+	m.style.display = "none";
+  }
+}
+
+function showSocial() {
+  var x = document.getElementById("social");
+  var y = document.getElementById("product-d");
+  var z = document.getElementById("software");
+  var m = document.getElementById("interim");
+  if (x.style.display === "none") {
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none";
+	m.style.display = "none";
+  } else {
+	x.style.display = "none";
+	y.style.display = "none";
+	z.style.display = "block";
+	m.style.display = "none";
+  }
+}
+
+function showInterim() {
+  var x = document.getElementById("interim");
+  var y = document.getElementById("product-d");
+  var z = document.getElementById("software");
+  var m = document.getElementById("social");
+  if (x.style.display === "none") {
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none";
+	m.style.display = "none";
+  } else {
+	x.style.display = "none";
+	y.style.display = "none";
+	z.style.display = "block";
+	m.style.display = "none";
+  }
+}
+
+</script>
+
 <?php get_footer(); ?>
