07d4643a48acdb9ca8cd49d87ad2278942f13bc4
hrso94 <hrvoje.medak@gluonsoftware.hr>
2020-05-28 10:48:30 +0200

    update on projects

--- projects.php
+++ projects.php
@@ -23,7 +23,7 @@
                     <h1 class="projectsh1">Republish</h1>
                     <p class="projects-page-p">A clean and stylish iOS applications that you can use to share interesting content with friends. No need to search for everybody because you are automatically connected with all your phone contacts, even if you don’t sign in! Forget about clutter and ads, just content in a beautiful, centralized timeline we are calling </p>
                     <div class="col-md-12 apple-btn-projects">
-                      <button class="apple_store_projects" style="margin-top:56px; margin-bottom:57px; width:100%;">
+                      <button class="apple_store_projects" style="margin-top:56px; margin-bottom:57px; width:100%;" onclick="window.location.href='https://apps.apple.com/hr/app/republish/id1460364335'">
                         <p class="apple_p">APPLE STORE</p>
                       </button>
                     </div>
@@ -40,7 +40,7 @@
                     <h1 class="projectsh1">Elude</h1>
 					<p class="projects-page-p">Travel on a budget, you define it and Elude app finds it. The Elude app lets you find the best travel arrangements within your budget by searching the best flights and hotels for the destination of your choosing.</p>
 					<div class="col-md-12 apple-btn-projects">
-                      <button class="apple_store_projects" style="margin-top:56px; margin-bottom:57px; width:100%;" onclick="window.location.href='https://apps.apple.com/hr/app/republish/id1460364335'">
+                      <button class="apple_store_projects" style="margin-top:56px; margin-bottom:57px; width:100%;">
                         <p class="apple_p">APPLE STORE</p>
                       </button>
                     </div>
