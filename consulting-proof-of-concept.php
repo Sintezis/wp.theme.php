65f2236b48d5bc1706c76cb73deea038da155658
Domagoj Bagaric <bagijaner@gmail.com>
2020-06-10 15:24:16 +0200

    fix

--- consulting-proof-of-concept.php
+++ consulting-proof-of-concept.php
@@ -1,5 +1,5 @@
 <?php get_header(); 
-/*Template Name: Consulting */
+/*Template Name: Consulting-proof-of-concept */
 ?>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
